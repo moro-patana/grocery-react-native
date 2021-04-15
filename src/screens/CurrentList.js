3ecf6b54f1839370fd029e992aeb0212dae882b5
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-04-15 14:17:48 +0300

    added the input

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,26 +1,47 @@
 import React from 'react'
 
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, FlatList, KeyboardAvoidingView } from 'react-native'
 
 import nachos from '../data/nachos'
 import ListItem, { Separator } from "../components/ListItem"
+import AddItem from "../components/AddItem"
 export default () => {
     console.log('Hello!');
     return (
-        <SafeAreaView>
-            <ScrollView>
-                {nachos.map((item, index)=> (
-                    <React.Fragment key={item.id} >
-                        <ListItem 
-                        name={item.name} 
-                        onFavoritePress={() => alert('todo: handle favorite!')}
-                        isFavorite={index < 2}
-                        />
-                        <Separator/>
-                    </React.Fragment>
-                ))}
-            </ScrollView>
+        <SafeAreaView style={{ flex: 1 }}>
+            <KeyboardAvoidingView 
+            style={{ flex: 1 }}
+            behavior='padding'
+            >
+                <FlatList
+                data={nachos}
+                renderItem={({ item, index }) => (
+                    <ListItem
+                    name={item.name} 
+                    onFavoritePress={() => alert('todo: handle favorite!')}
+                    isFavorite={index < 2}
+                    />
+                )}
+                keyExtractor={(item) => item.id}
+                ItemSeparatorComponent={() => <Separator />}
+                ListHeaderComponent={() => <AddItem />}
+                />
+            </KeyboardAvoidingView>
         </SafeAreaView>
+        // <SafeAreaView>
+        //     <ScrollView>
+        //         {nachos.map((item, index)=> (
+        //             <React.Fragment key={item.id} >
+        //                 <ListItem 
+        //                 name={item.name} 
+        //                 onFavoritePress={() => alert('todo: handle favorite!')}
+        //                 isFavorite={index < 2}
+        //                 />
+        //                 <Separator/>
+        //             </React.Fragment>
+        //         ))}
+        //     </ScrollView>
+        // </SafeAreaView>
 
     )
 
