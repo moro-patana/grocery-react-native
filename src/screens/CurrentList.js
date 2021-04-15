00c669eb95ae2cb3baea389291ec26f693cfee64
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-04-15 09:52:11 +0300

    imported the list from the ListItem component

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,16 +1,21 @@
 import React from 'react'
 
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 
 import nachos from '../data/nachos'
+import ListItem, { Separator } from "../components/ListItem"
 export default () => {
+    console.log('Hello!');
     return (
         <SafeAreaView>
-            {nachos.map((item)=>{
-
-                return <Text key={item.id}>{item.name}</Text>
-
-            })}
+            <ScrollView>
+                {nachos.map((item)=> (
+                    <React.Fragment key={item.id} >
+                        <ListItem name={item.name}/>
+                        <Separator/>
+                    </React.Fragment>
+                ))}
+            </ScrollView>
         </SafeAreaView>
 
     )
