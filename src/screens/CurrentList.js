c22801338bf59b43b145793cc77f3accec4d9223
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-04-15 14:38:39 +0300

    set state for the list and added onSubmit to the input

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { View, Text, SafeAreaView, ScrollView, FlatList, KeyboardAvoidingView } from 'react-native'
-
+import { View, Text, SafeAreaView, ScrollView, FlatList, KeyboardAvoidingView, SectionList } from 'react-native'
+import {v4 as uuid } from 'uuid';
 import nachos from '../data/nachos'
 import ListItem, { Separator } from "../components/ListItem"
 import AddItem from "../components/AddItem"
 export default () => {
     console.log('Hello!');
+    const [list, setList] = useState(nachos)
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <KeyboardAvoidingView 
@@ -14,7 +15,7 @@ export default () => {
             behavior='padding'
             >
                 <FlatList
-                data={nachos}
+                data={list}
                 renderItem={({ item, index }) => (
                     <ListItem
                     name={item.name} 
@@ -24,7 +25,10 @@ export default () => {
                 )}
                 keyExtractor={(item) => item.id}
                 ItemSeparatorComponent={() => <Separator />}
-                ListHeaderComponent={() => <AddItem />}
+                ListHeaderComponent={() => <AddItem 
+                    onSubmitEditing={({ nativeEvent: { text }}) => {
+                    setList([{id: uuid(), name: text}, ...list])
+                }}/>}
                 />
             </KeyboardAvoidingView>
         </SafeAreaView>
